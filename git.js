86d4f2d8b12289bd3d980d5107652dd3c0b1e7ed
Mizan <mizan_98@hotmail.co.uk>
2019-10-14 10:46:55 +0100

    my name is Mizan

--- git.js
+++ git.js
@@ -1,1 +1,16 @@
 console.log("My name is Mizan")
+
+/////////////////////////////GIT/////////////////////////////
+
+//git allows us to track our activity to track it 
+
+//use git add and then the name of the file to track a file 
+
+//use git status after track to see if the file has ben modified 
+
+//to create a save with git do: git commit -m " whatever i wanted to save"
+
+//to set up your own name as the auther and emaim address as you do the following: git config --global user.name "Mizan"
+                                                                                // git config --global user.email mizan_98@hotmail.co.uk
+
+//to push files to github do :git push -u origin master
